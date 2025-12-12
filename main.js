cbac924e76d0ea43a40434c4c881fd8401ad61de
caspa <example@example.com>
2025-12-12 21:24:33 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+let candidateDescription = null
+
+let dataChannel = null
+
+const handleMessage = (event) => {
+    console.log(event.data)
+}
+
+const init = async () => {
+    const connection = new RTCPeerConnection()
+
+    dataChannel = await connection.createDataChannel('test channel')
+
+    dataChannel.onmessage = handleMessage
+
+    const offer = await connection.createOffer({
+        offerToReceiveAudio: false,
+        offerToReceiveVideo: false,
+    })
+
+    await connection.setLocalDescription(offer)
+
+    connection.onconnectionstatechange = async (state) => {
+        console.log(state.currentTarget.connectionState)
+    }
+
+    connection.onicecandidate = (event) => {
+        if (event.candidate && !candidateDescription) {
+            connection.addIceCandidate(event.candidate)
+            candidateDescription = event.candidate.candidate
+
+            console.log(
+                JSON.stringify(connection.localDescription) +
+                    'POOPY_BALLS' +
+                    candidateDescription
+            )
+        }
+    }
+
+    return connection
+}
+
+const handleInput = async (jsonString, connection) => {
+    const desc = JSON.parse(jsonString)
+
+    await connection.setRemoteDescription(desc)
+}
+
+init().then((connection) => {
+    document.getElementById('button').addEventListener('click', () => {
+        handleInput(document.getElementById('input').value, connection)
+    })
+    document.getElementById('buzz').addEventListener('click', () => {
+        dataChannel?.send('buzz buzz')
+    })
+})
